100b0750c402883079d2cb6ef1ab79feddcdbf8a
Duncan Butler <duncan@intrepidowls.com>
2021-07-02 13:46:15 +0000

    Add styles

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,15 +1,15 @@
 module PhotoGroove exposing (main)
 
 import Html exposing (div, h1, img, text)
-import Html.Attributes exposing (...)
+import Html.Attributes exposing (..)
 
 view model = 
     div [ class "content" ]
     [h1 [] [text "Photo Groove" ]
         , div [ id "thumbnails" ]
-        [img [src "http://elm-in-action.com/1.jpg"] []
-        , img [src "http://elm-in-action.com/2.jpg"] []
-        , img [src "http://elm-in-action.com/3.jpg"] []
+        [img [src "http://elm-in-action.com/1.jpeg"] []
+        , img [src "http://elm-in-action.com/2.jpeg"] []
+        , img [src "http://elm-in-action.com/3.jpeg"] []
         ]
     ]
 
